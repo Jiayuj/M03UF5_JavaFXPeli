f4c1a7b6f5d2de001c1708069a6548fb599d5114
dam2a <amateu@elpuig.xeill.net>
2020-02-07 17:50:37 +0100

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.*;
 
 
 import javax.xml.bind.JAXB;
+import java.awt.*;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -25,11 +30,27 @@ public class Main extends Application {
     static final String sessionURL ="http://www.gencat.cat/llengua/cinema/film_sessions.xml";
     static List<Session> sessions;
 
+    private TableView table = new TableView();
+
 
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+
         stage.setTitle("App");
+
+        table.setEditable(true);
+
+        TableColumn cineIdCol = new TableColumn("Cine_ID");
+        TableColumn cineCol = new TableColumn("Cine");
+        TableColumn cineAdrecaCol = new TableColumn("Cine_Adreça");
+        TableColumn localitatCol = new TableColumn("Cine_Adreça");
+        TableColumn comarcaCol = new TableColumn("Cine_Adreça");
+        TableColumn provinciaCol = new TableColumn("Cine_Adreça");
+
+
+        table.getColumns().addAll(cineIdCol, cineCol, cineAdrecaCol, localitatCol, comarcaCol, provinciaCol);
+
         stage.setScene(new Scene(root, 500, 510));
         stage.show();
     }
